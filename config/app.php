efe4fce4586e05e0fbd64778dda89c2a331bdd87
Riaz <mriyadhazhar@gmail.com>
2024-05-24 09:29:58 +0700

    nothing

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/keistimewaan-kecil-main/public/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
